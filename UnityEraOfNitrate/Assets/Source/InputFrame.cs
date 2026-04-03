c2c089d97f06a9cf7dd4521dc1dfdbb0b1c35d11
Jong-il Hong <86797396+jihworks@users.noreply.github.com>
2026-04-03 15:24:36 +0900

    Initial project

---
+++ UnityEraOfNitrate/Assets/Source/InputFrame.cs
@@ -0,0 +1,33 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using Jih.Unity.Infrastructure.Runtime;
+
+namespace Jih.Unity.EraOfNitrate
+{
+    public class InputFrame : InputFrame<InputSystem_Actions>
+    {
+        public bool Player { get; }
+
+        public InputFrame(object holder, bool ui, bool player) : base(holder, ui)
+        {
+            Player = player;
+        }
+
+        protected override void DoApply(InputSystem_Actions inputActions)
+        {
+            if (Player)
+            {
+                inputActions.Player.Enable();
+            }
+            else
+            {
+                inputActions.Player.Disable();
+            }
+        }
+    }
+}
---
+++ UnityEraOfNitrate/Assets/Source/Main.State.cs
@@ -0,0 +1,32 @@
+﻿// © 2026 Jong-il Hong
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+// SPDX-License-Identifier: MIT
+
+#nullable enable
+
+using Jih.Unity.Infrastructure.Runtime;
+
+namespace Jih.Unity.EraOfNitrate
+{
+    public partial class Main
+    {
+        abstract class State : StateBase<Main>
+        {
+            public State(Main owner) : base(owner)
+            {
+            }
+
+            public virtual void FixedUpdate()
+            {
+            }
+        }
+
+        class Sleep : State
+        {
+            public Sleep(Main owner) : base(owner)
+            {
+            }
+        }
+    }
+}
